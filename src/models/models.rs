2906a700045e5793e0c1c08df77adbb866c4f992
mutalisk999 <81849682@qq.com>
2023-09-15 16:44:50 +0800

    add model Product

--- src/models/models.rs
+++ src/models/models.rs
@@ -7,15 +7,35 @@ use rust_decimal::prelude::*;
 
 use crate::models::types::*;
 
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct Product {
+    pub id: u64,
+    #[serde(with = "chrono::serde::ts_nanoseconds")]
+    pub created_at: DateTime<Utc>,
+    #[serde(with = "chrono::serde::ts_nanoseconds")]
+    pub updated_at: DateTime<Utc>,
+    pub base_currency: String,
+    pub quote_currency: String,
+    pub base_min_size: Decimal,
+    pub base_max_size: Decimal,
+    pub quote_min_size: Decimal,
+    pub quote_max_size: Decimal,
+    pub base_scale: i32,
+    pub quote_scale: i32,
+    pub quote_increment: Decimal,
+}
+
+
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Order {
-    pub id: i64,
+    pub id: u64,
     #[serde(with = "chrono::serde::ts_nanoseconds")]
     pub created_at: DateTime<Utc>,
     #[serde(with = "chrono::serde::ts_nanoseconds")]
     pub updated_at: DateTime<Utc>,
     pub product_id: String,
-    pub user_id: i64,
+    pub user_id: u64,
     pub client_oid: String,
     pub size: Decimal,
     pub funds: Decimal,
