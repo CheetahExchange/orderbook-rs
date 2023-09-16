3256d5ca0cd109fdf86be19964844bf9dccf8efb
mutalisk999 <81849682@qq.com>
2023-09-16 22:22:33 +0800

    add function is_order_will_not_match for struct OrderBook

--- src/matching/order_book.rs
+++ src/matching/order_book.rs
@@ -1,5 +1,3 @@
-use std::collections::BTreeMap;
-use std::iter::Map;
 // #[macro_use]
 use serde::{Deserialize, Serialize};
 use serde_json;
@@ -7,9 +5,14 @@ use serde_json;
 use crate::matching::ordering::{PriceOrderIdKeyAsc, PriceOrderIdKeyDesc};
 use rust_decimal::Decimal;
 
+use std::collections::BTreeMap;
+use std::collections::HashMap;
+
 use crate::models::models::{Order, Product};
 use crate::models::types::{OrderType, Side, TimeInForceType};
 use crate::utils::window::Window;
+
+const ORDER_ID_WINDOW_CAP: u64 = 10000;
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct BookOrder {
@@ -23,16 +26,18 @@ pub struct BookOrder {
     pub time_in_force: TimeInForceType,
 }
 
-pub fn new_book_order(order: Order) -> BookOrder {
-    BookOrder {
-        order_id: order.id,
-        user_id: order.user_id,
-        size: order.size,
-        funds: order.funds,
-        price: order.price,
-        side: order.side,
-        r#type: order.r#type,
-        time_in_force: order.time_in_force,
+impl BookOrder {
+    pub fn new_book_order(order: Order) -> Self {
+        BookOrder {
+            order_id: order.id,
+            user_id: order.user_id,
+            size: order.size,
+            funds: order.funds,
+            price: order.price,
+            side: order.side,
+            r#type: order.r#type,
+            time_in_force: order.time_in_force,
+        }
     }
 }
 
@@ -46,12 +51,12 @@ pub struct OrderBookSnapshot {
 }
 
 pub struct AskDepth {
-    pub orders: Map<u64, BookOrder>,
+    pub orders: HashMap<u64, BookOrder>,
     pub queue: BTreeMap<PriceOrderIdKeyAsc, u64>,
 }
 
 pub struct BidDepth {
-    pub orders: Map<u64, BookOrder>,
+    pub orders: HashMap<u64, BookOrder>,
     pub queue: BTreeMap<PriceOrderIdKeyDesc, u64>,
 }
 
@@ -62,4 +67,61 @@ pub struct OrderBook {
     pub trade_seq: u64,
     pub log_seq: u64,
     pub order_id_window: Window,
+}
+
+impl OrderBook {
+    pub fn new_order_book(product: Product) -> Self {
+        OrderBook {
+            product,
+            ask_depths: AskDepth {
+                orders: HashMap::<u64, BookOrder>::new(),
+                queue: BTreeMap::<PriceOrderIdKeyAsc, u64>::new(),
+            },
+            bid_depths: BidDepth {
+                orders: HashMap::<u64, BookOrder>::new(),
+                queue: BTreeMap::<PriceOrderIdKeyDesc, u64>::new(),
+            },
+            trade_seq: 0,
+            log_seq: 0,
+            order_id_window: Window::new(0, ORDER_ID_WINDOW_CAP),
+        }
+    }
+
+    pub fn is_order_will_not_match(&self, order: Order) -> bool {
+        let mut taker_order = BookOrder::new_book_order(order);
+        match taker_order.r#type {
+            OrderType::OrderTypeMarket => {
+                taker_order.price = match taker_order.side {
+                    Side::SideBuy => Decimal::MAX,
+                    Side::SideSell => Decimal::ZERO,
+                }
+            }
+            _ => {}
+        }
+
+        return match taker_order.side {
+            Side::SideBuy => match self.ask_depths.queue.first_key_value() {
+                None => true,
+                Some((k, v)) => {
+                    let maker_order = self.ask_depths.orders.get(v).unwrap();
+                    if taker_order.price.lt(&maker_order.price) {
+                        true
+                    } else {
+                        false
+                    }
+                }
+            },
+            Side::SideSell => match self.bid_depths.queue.first_key_value() {
+                None => true,
+                Some((k, v)) => {
+                    let maker_order = self.bid_depths.orders.get(v).unwrap();
+                    if taker_order.price.gt(&maker_order.price) {
+                        true
+                    } else {
+                        false
+                    }
+                }
+            },
+        };
+    }
 }
